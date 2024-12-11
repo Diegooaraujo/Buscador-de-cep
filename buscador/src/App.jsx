541ed96795898f682a1892cd7b1d3ff16beb420c
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-12-11 19:54:01 -0300

    full code

--- buscador/src/App.jsx
+++ buscador/src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import api from './services/api'
+
+import {FiSearch} from 'react-icons/fi'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+  async function handSearch(){
+    if(input == ""){
+      alert("preencha algum cep")
+      return;
+    }else{
+      try{
+        const response = await api.get(`${input}/json/`)
+        setCep(response.data)
+        setInput("")
+      }catch{
+        alert("Ops, erro ao buscar")
+        setInput("")
+      }
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='conteiner'>
+        <h1 className='title'>Buscador CEP</h1>
+
+        <div className='conteinerInput'>
+          <input type="text" placeholder='Digite seu CEP' value={input} onChange={(e)=>setInput(e.target.value)} />
+          <button className='buttonSearch' onClick={handSearch}><FiSearch size={25} color='FFF' /></button>
+        </div>
+
+        {Object.keys(cep).length > 0 &&(
+           <main className='main'>
+           <h2>CEP: {cep.cep}</h2>
+           <span>Rua: {cep.logradouro} </span>
+           <span>complemento: {cep.complemento}</span>
+           <span>Bairro: {cep.bairro}</span>
+           <span>Localidade: {cep.localidade} - {cep.uf}</span>
+           <span>ddd: {cep.ddd}</span>
+         </main>
+        )}
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
